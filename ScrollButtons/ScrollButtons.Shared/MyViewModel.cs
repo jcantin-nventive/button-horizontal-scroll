7d2e0228c98338030d524a6d570e6c004deb4405
Julie Cantin <julie.cantin@nventive.com>
2019-12-10 11:57:31 -0500

    bug reproduced with listview

---
+++ ScrollButtons/ScrollButtons.Shared/MyViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ScrollButtons
+{
+	public class MyViewModel
+	{
+		public IEnumerable<string> MyItems => Enumerable.Range(1, 5).Select(nb => $"Item #{nb}");
+	}
+}
